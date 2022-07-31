f7d0baea6deeb7af032ea8b4a5daa5af6bd62f4a
Moo <tinnate@live.com>
2022-07-31 20:56:31 +0800

    add Fox

---
+++ Form/Fox.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using Machina.FFXIV;
+using Machina.Infrastructure;
+
+namespace WPF_learning_notes.Form;
+
+public partial class Fox
+{
+    public Fox()
+    {
+        InitializeComponent();
+    }
+
+    private readonly FFXIVNetworkMonitor _monitor = new();
+    private readonly FFXIVBundleDecoder _decoder = new();
+
+    private void Frame_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        _monitor.ProcessID = (uint)GetFfxivProcess().Id;
+        _monitor.WindowName = GetFfxivProcess().MainWindowTitle;
+        _monitor.OodlePath = GetFfxivProcess().MainModule!.FileName;
+        _monitor.MessageReceivedEventHandler = MessageReceived;
+        _monitor.MessageSentEventHandler = MessageSent;
+        _monitor.Start();
+    }
+
+    private void MessageSent(TCPConnection connection, long epoch, byte[] message)
+    {
+        _decoder.StoreData(message);
+    }
+
+    private void MessageReceived(TCPConnection connection, long epoch, byte[] message)
+    {
+        _decoder.StoreData(message);
+    }
+
+
+    private static Process GetFfxivProcess()
+    {
+        var processes = Process.GetProcessesByName("ffxiv_dx11");
+        return processes[0];
+    }
+
+    private void Frame_OnClosed(object? sender, EventArgs e)
+    {
+        _monitor.Stop();
+    }
+}
